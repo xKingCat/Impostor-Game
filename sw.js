86951e550f066c4f5aef1a48a38bfb78de69d089
xRu <104589956+xKingCat@users.noreply.github.com>
2026-05-06 17:24:30 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = "undercover-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache =>
+      cache.addAll(["./", "./index.html"])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
